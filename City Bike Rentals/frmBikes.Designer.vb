e9b902fd2dc994b12f71374cf73dc544bbd43298
ttwoosta <ttwoosta@gmail.com>
2017-10-06 16:56:44 -0400

    Modified Form size and control positions.

--- City Bike Rentals/frmBikes.Designer.vb
+++ City Bike Rentals/frmBikes.Designer.vb
@@ -37,7 +37,8 @@ Partial Class frmBikes
         '
         'picBikes
         '
-        Me.picBikes.Location = New System.Drawing.Point(0, 278)
+        Me.picBikes.Location = New System.Drawing.Point(1, 287)
+        Me.picBikes.Margin = New System.Windows.Forms.Padding(2)
         Me.picBikes.Name = "picBikes"
         Me.picBikes.Size = New System.Drawing.Size(407, 175)
         Me.picBikes.TabIndex = 0
@@ -47,9 +48,10 @@ Partial Class frmBikes
         '
         Me.lblHeading.AutoSize = True
         Me.lblHeading.Font = New System.Drawing.Font("Cooper Black", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblHeading.Location = New System.Drawing.Point(62, 9)
+        Me.lblHeading.Location = New System.Drawing.Point(85, 30)
+        Me.lblHeading.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblHeading.Name = "lblHeading"
-        Me.lblHeading.Size = New System.Drawing.Size(283, 35)
+        Me.lblHeading.Size = New System.Drawing.Size(226, 27)
         Me.lblHeading.TabIndex = 1
         Me.lblHeading.Text = "City Bike Rentals"
         '
@@ -57,9 +59,10 @@ Partial Class frmBikes
         '
         Me.lblCostHeading.AutoSize = True
         Me.lblCostHeading.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblCostHeading.Location = New System.Drawing.Point(75, 44)
+        Me.lblCostHeading.Location = New System.Drawing.Point(86, 66)
+        Me.lblCostHeading.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblCostHeading.Name = "lblCostHeading"
-        Me.lblCostHeading.Size = New System.Drawing.Size(257, 25)
+        Me.lblCostHeading.Size = New System.Drawing.Size(209, 20)
         Me.lblCostHeading.TabIndex = 2
         Me.lblCostHeading.Text = "$X.XX per Bike for 24 Hours"
         '
@@ -67,18 +70,20 @@ Partial Class frmBikes
         '
         Me.lblNumberOfBikes.AutoSize = True
         Me.lblNumberOfBikes.Font = New System.Drawing.Font("Century Gothic", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblNumberOfBikes.Location = New System.Drawing.Point(25, 89)
+        Me.lblNumberOfBikes.Location = New System.Drawing.Point(70, 114)
+        Me.lblNumberOfBikes.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblNumberOfBikes.Name = "lblNumberOfBikes"
-        Me.lblNumberOfBikes.Size = New System.Drawing.Size(224, 22)
+        Me.lblNumberOfBikes.Size = New System.Drawing.Size(180, 20)
         Me.lblNumberOfBikes.TabIndex = 3
         Me.lblNumberOfBikes.Text = "Number of Bike Rentals:"
         '
         'txtNumnerOfBikes
         '
         Me.txtNumnerOfBikes.Font = New System.Drawing.Font("Century Gothic", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtNumnerOfBikes.Location = New System.Drawing.Point(305, 86)
+        Me.txtNumnerOfBikes.Location = New System.Drawing.Point(281, 111)
+        Me.txtNumnerOfBikes.Margin = New System.Windows.Forms.Padding(2)
         Me.txtNumnerOfBikes.Name = "txtNumnerOfBikes"
-        Me.txtNumnerOfBikes.Size = New System.Drawing.Size(39, 30)
+        Me.txtNumnerOfBikes.Size = New System.Drawing.Size(30, 25)
         Me.txtNumnerOfBikes.TabIndex = 4
         Me.txtNumnerOfBikes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
@@ -86,9 +91,10 @@ Partial Class frmBikes
         '
         Me.lblTotalCostLabel.AutoSize = True
         Me.lblTotalCostLabel.Font = New System.Drawing.Font("Century Gothic", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblTotalCostLabel.Location = New System.Drawing.Point(25, 134)
+        Me.lblTotalCostLabel.Location = New System.Drawing.Point(70, 147)
+        Me.lblTotalCostLabel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblTotalCostLabel.Name = "lblTotalCostLabel"
-        Me.lblTotalCostLabel.Size = New System.Drawing.Size(248, 22)
+        Me.lblTotalCostLabel.Size = New System.Drawing.Size(199, 20)
         Me.lblTotalCostLabel.TabIndex = 5
         Me.lblTotalCostLabel.Text = "Total Cost of Bikes Rented:"
         '
@@ -96,18 +102,20 @@ Partial Class frmBikes
         '
         Me.lblTotalCost.AutoSize = True
         Me.lblTotalCost.Font = New System.Drawing.Font("Century Gothic", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblTotalCost.Location = New System.Drawing.Point(301, 134)
+        Me.lblTotalCost.Location = New System.Drawing.Point(277, 147)
+        Me.lblTotalCost.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblTotalCost.Name = "lblTotalCost"
-        Me.lblTotalCost.Size = New System.Drawing.Size(81, 22)
+        Me.lblTotalCost.Size = New System.Drawing.Size(61, 20)
         Me.lblTotalCost.TabIndex = 6
         Me.lblTotalCost.Text = "$888.88"
         '
         'btnCalculate
         '
         Me.btnCalculate.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnCalculate.Location = New System.Drawing.Point(5, 215)
+        Me.btnCalculate.Location = New System.Drawing.Point(38, 219)
+        Me.btnCalculate.Margin = New System.Windows.Forms.Padding(2)
         Me.btnCalculate.Name = "btnCalculate"
-        Me.btnCalculate.Size = New System.Drawing.Size(125, 27)
+        Me.btnCalculate.Size = New System.Drawing.Size(98, 22)
         Me.btnCalculate.TabIndex = 7
         Me.btnCalculate.Text = "Calculate Cost"
         Me.btnCalculate.UseVisualStyleBackColor = True
@@ -115,9 +123,10 @@ Partial Class frmBikes
         'btnClear
         '
         Me.btnClear.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnClear.Location = New System.Drawing.Point(136, 215)
+        Me.btnClear.Location = New System.Drawing.Point(155, 219)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(2)
         Me.btnClear.Name = "btnClear"
-        Me.btnClear.Size = New System.Drawing.Size(130, 27)
+        Me.btnClear.Size = New System.Drawing.Size(98, 22)
         Me.btnClear.TabIndex = 8
         Me.btnClear.Text = "Clear"
         Me.btnClear.UseVisualStyleBackColor = True
@@ -125,18 +134,19 @@ Partial Class frmBikes
         'btnExit
         '
         Me.btnExit.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnExit.Location = New System.Drawing.Point(272, 215)
+        Me.btnExit.Location = New System.Drawing.Point(272, 219)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(2)
         Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(130, 27)
+        Me.btnExit.Size = New System.Drawing.Size(98, 22)
         Me.btnExit.TabIndex = 9
         Me.btnExit.Text = "Exit"
         Me.btnExit.UseVisualStyleBackColor = True
         '
         'frmBikes
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(407, 453)
+        Me.ClientSize = New System.Drawing.Size(409, 461)
         Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.btnClear)
         Me.Controls.Add(Me.btnCalculate)
@@ -147,6 +157,7 @@ Partial Class frmBikes
         Me.Controls.Add(Me.lblCostHeading)
         Me.Controls.Add(Me.lblHeading)
         Me.Controls.Add(Me.picBikes)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "frmBikes"
         Me.Text = "City Bike Rentals"
         CType(Me.picBikes, System.ComponentModel.ISupportInitialize).EndInit()
